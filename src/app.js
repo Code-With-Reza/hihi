5ec12bb77e09ea53f88055fc741461ab2537510e
Reza <97615448+Code-With-Reza@users.noreply.github.com>
2024-02-19 07:57:19 +0700

    please luck

--- src/app.js
+++ src/app.js
@@ -1,45 +1,43 @@
-const express = require('express')
-const fetch = require('node-fetch')
+const express = require('express');
+const fetch = require('node-fetch');
+const app = express();
+const cache = require('./cache');
 
-const cache = require('./cache')
-
-const app = express()
-
-const reChannelName = /"owner":{"videoOwnerRenderer":{"thumbnail":{"thumbnails":\[.*?\]},"title":{"runs":\[{"text":"(.+?)"/
+const reChannelName = /"owner":{"videoOwnerRenderer":{"thumbnail":{"thumbnails":\[.*?\]},"title":{"runs":\[{"text":"(.+?)"/;
 
 const getLiveStream = async (url) => {
-  let data = await cache?.get(url)
+  let data = await cache?.get(url);
 
   if (data) {
-    return JSON.parse(data)
+    return JSON.parse(data);
   } else {
-    data = {}
+    data = {};
 
     try {
-      const response = await fetch(url)
+      const response = await fetch(url);
 
       if (response.ok) {
-        const text = await response.text()
-        const stream = text.match(/(?<=hlsManifestUrl":").*\.m3u8/)?.[0]
-        const name = reChannelName.exec(text)?.[1]
-        const logo = text.match(/(?<=owner":{"videoOwnerRenderer":{"thumbnail":{"thumbnails":\[{"url":")[^=]*/)?.[0]
+        const text = await response.text();
+        const stream = text.match(/(?<=hlsManifestUrl":").*\.m3u8/)?.[0];
+        const name = reChannelName.exec(text)?.[1];
+        const logo = text.match(/(?<=owner":{"videoOwnerRenderer":{"thumbnail":{"thumbnails":\[{"url":")[^=]*/)?.[0];
 
-        data = { name, stream, logo }
+        data = { name, stream, logo };
       } else {
         console.log(JSON.stringify({
           url,
           status: response.status
-        }))
+        }));
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
 
-    await cache?.set(url, JSON.stringify(data), { EX: 300 })
+    await cache?.set(url, JSON.stringify(data), { EX: 300 });
 
-    return data
+    return data;
   }
-}
+};
 
 app.use(require('express-status-monitor')());
 
@@ -49,14 +47,13 @@ app.use(function(req, res, next) {
   next();
 });
 
-
 app.get('/', (req, res, nxt) => {
   try {
-    res.json({ message: 'Status OK' })
+    res.json({ message: 'Status OK' });
   } catch (err) {
-    nxt(err)
+    nxt(err);
   }
-})
+});
 
 app.get('/channel/:id.m3u8', async (req, res, nxt) => {
   try {
@@ -65,9 +62,10 @@ app.get('/channel/:id.m3u8', async (req, res, nxt) => {
 
     if (stream) {
       // Proxy the stream
-      const response = await axios.get(stream, { responseType: 'stream' });
-      res.set(response.headers);
-      response.data.pipe(res);
+      const response = await fetch(stream);
+      const streamHeaders = response.headers;
+      res.set(streamHeaders);
+      response.body.pipe(res);
     } else {
       // No stream found
       res.sendStatus(204);
@@ -85,9 +83,10 @@ app.get('/video/:id.m3u8', async (req, res, nxt) => {
 
     if (stream) {
       // Proxy the stream
-      const response = await axios.get(stream, { responseType: 'stream' });
-      res.set(response.headers);
-      response.data.pipe(res);
+      const response = await fetch(stream);
+      const streamHeaders = response.headers;
+      res.set(streamHeaders);
+      response.body.pipe(res);
     } else {
       // No stream found
       res.sendStatus(204);
@@ -99,28 +98,30 @@ app.get('/video/:id.m3u8', async (req, res, nxt) => {
 
 app.get('/cache', async (req, res, nxt) => {
   try {
-    const keys = await cache?.keys('*')
+    const keys = await cache?.keys('*');
 
-    const items = []
+    const items = [];
 
     for (const key of keys) {
-      const data = JSON.parse(await cache?.get(key))
+      const data = JSON.parse(await cache?.get(key));
 
       if (data) {
         items.push({
           url: key,
           name: data.name,
           logo: data.logo
-        })
+        });
       }
     }
 
-    res.json(items)
-  } catch (err) { nxt(err) }
-})
+    res.json(items);
+  } catch (err) { 
+    nxt(err);
+  }
+});
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
-  console.log(`express app (node ${process.version}) is running on port ${port}`)
-})
+  console.log(`express app (node ${process.version}) is running on port ${port}`);
+});
